11ddbe65ba2323514c002829ca2f4c3f812fe928
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-24 15:01:08 +0800

    08241501

--- BradJava/src/tw/brad/tutor/Brad06.java
+++ BradJava/src/tw/brad/tutor/Brad06.java
@@ -1,23 +1,36 @@
 package tw.brad.tutor;
 
+import java.util.Scanner;
+
 public class Brad06 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Year = ");
+		int year = scanner.nextInt();
+		//System.out.println(year);
 		
-		int year = 2026;
+		boolean isLeap;
+		
 		if (year % 4 == 0) {
 			if (year % 100 == 0) {
 				if (year % 400 == 0) {
 					// 29
+					isLeap = true;
 				}else {
 					// 28
+					isLeap = false;
 				}
 			}else {
 				// 29
+				isLeap = true;
 			}
 		}else {
 			// 28
+			isLeap = false;
 		}
+		
+		System.out.printf("%d年是%s年", year, isLeap?"潤":"平");
 		
 		
 		
