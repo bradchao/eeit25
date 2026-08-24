1b96d7679a07bf2371adb1cb6c9981cfb1a3cfc3
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-24 14:09:25 +0800

    08241409

--- BradJava/src/tw/brad/tutor/Brad05.java
+++ BradJava/src/tw/brad/tutor/Brad05.java
@@ -3,8 +3,14 @@ package tw.brad.tutor;
 public class Brad05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double temp = Math.random();
+		int score = (int)(temp * 101);
+		System.out.println(score);
+		if (score >= 60) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Down");
+		}
 	}
 
 }
