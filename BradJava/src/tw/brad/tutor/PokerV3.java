81ba0091e0cf288e0bb928ac54fd5d35b2034761
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-25 13:56:22 +0800

    08251356

--- BradJava/src/tw/brad/tutor/PokerV3.java
+++ BradJava/src/tw/brad/tutor/PokerV3.java
@@ -3,19 +3,41 @@ package tw.brad.tutor;
 public class PokerV3 {
 
 	public static void main(String[] args) {
-		int[] poker = new int[10];
+		int[] poker = new int[52];
 		for (int i=0; i<poker.length; i++) poker[i] = i;
 		
 		for (int i=0; i<poker.length; i++) {
-			int rIndex = (int)(Math.random()*10);
+			int rIndex = (int)(Math.random()*52);
 			// poker[i] <-> poker[rIndex]
 			int temp = poker[i];
 			poker[i] = poker[rIndex];
 			poker[rIndex] = temp; 
 		}
-		for (int card :poker) {
-			System.out.println(card);
+//		for (int card :poker) {
+//			System.out.println(card);
+//		}
+//		System.out.println("---");
+		//---------------------------
+		int[][] players = new int[4][13];
+		for (int i=0; i<poker.length; i++) {
+			players[i%4][i/4] = poker[i];
 		}
+		
+		for (int[] player: players) {
+			for (int card : player) {
+				System.out.print(card + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
