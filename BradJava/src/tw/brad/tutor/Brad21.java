5549bdacdfc3b09494b2cf93dfb0a80ee78da6b3
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-27 11:16:24 +0800

    08271116

--- BradJava/src/tw/brad/tutor/Brad21.java
+++ BradJava/src/tw/brad/tutor/Brad21.java
@@ -3,8 +3,11 @@ package tw.brad.tutor;
 public class Brad21 {
 	public static void main(String[] args) {
 		Brad212 obj1 = new Brad212();
+		
 		Brad212 obj2 = new Brad213();
 		obj2.m1(); obj2.m2();
+		//obj2.m3();
+		
 		Brad213 obj3 = (Brad213)obj2;
 		obj3.m3();
 		
@@ -14,10 +17,16 @@ public class Brad21 {
 			System.out.println("No");
 			//Brad213 obj4 = (Brad213)obj1;
 		}
-		System.out.println("---");
+		System.out.println("---1");
 		Brad211 obj5 = new Brad214();
 		obj5.m1();
-		Brad212 obj6 = (Brad212)obj5;
+		//Brad212 obj6 = (Brad212)obj5;
+		System.out.println("-----2");
+		go(obj1);go(obj2);go(obj3);
+	}
+	
+	static void go(Brad212 obj1) {
+		obj1.m2();
 	}
 }
 class Brad211 {
---
+++ BradJava/src/tw/brad/tutor/GuessNumber.java
@@ -0,0 +1,43 @@
+package tw.brad.tutor;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class GuessNumber extends JFrame{
+	private JTextField input;
+	private JButton guess;
+	private JTextArea log;
+	
+	public GuessNumber() {
+		super("猜數字遊戲");
+		
+		//input = new JFormattedTextField();
+		input = new JTextField();
+		guess = new JButton("猜");
+		log = new JTextArea();
+		
+		setLayout(new BorderLayout(4, 4));
+		add(log, BorderLayout.CENTER);
+		
+		JPanel top = new JPanel(new BorderLayout());
+		top.add(guess, BorderLayout.EAST);
+		top.add(input, BorderLayout.CENTER);
+		
+		add(top, BorderLayout.NORTH);
+		
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public static void main(String[] args) {
+		new GuessNumber();
+	}
+
+}
