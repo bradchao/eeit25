50dc31c01dee8d79c2dfdc1ba93db3c924f1d53d
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-28 09:20:29 +0800

    08280920

--- BradJava/src/tw/brad/tutor/GuessNumber.java
+++ BradJava/src/tw/brad/tutor/GuessNumber.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -15,6 +16,7 @@ public class GuessNumber extends JFrame implements ActionListener{
 	private JButton guess;
 	private JTextArea log;
 	private String answer;
+	private int counter;
 	
 	public GuessNumber() {
 		super("猜數字遊戲");
@@ -49,17 +51,32 @@ public class GuessNumber extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		//System.out.println("OK");
 		//System.out.println(createAnswer(4));
+		
+		counter++;
+		
 		String g = input.getText();
-		String result = checkAB(g);
-		log.append(String.format("%s => %s\n", g, result));
+		String result = checkAB(answer, g);
+		log.append(String.format("%d. %s => %s\n", counter, g, result));
 		input.setText("");
+		
+		if (result.equals("3A0B")) {
+			JOptionPane.showMessageDialog(null, "WINNER");
+			initGame();
+		}else if (counter == 3) {
+			JOptionPane.showMessageDialog(null, "Answer = " + answer);
+			initGame();
+		}
+		
 	}
 	
 	private void initGame() {
 		answer = createAnswer(3);
+		System.out.println(answer);
+		counter = 0;
+		log.setText("");
 	}
 	
-	private String createAnswer(int d) {
+	private static String createAnswer(int d) {
 		int[] poker = new int[10];
 		for (int i=0; i<poker.length; i++) poker[i] = i;
 		
@@ -76,8 +93,16 @@ public class GuessNumber extends JFrame implements ActionListener{
 		return sb.toString();
 	}
 	
-	private String checkAB(String g) {
-		return "1A1B";
+	private static String checkAB(String answer, String g) {
+		int A, B; A = B = 0;
+		for (int i=0; i<answer.length(); i++) {
+			if (answer.charAt(i) == g.charAt(i)) {
+				A++;
+			}else if (answer.indexOf(g.charAt(i)) != -1) {
+				B++;
+			}
+		}
+		return String.format("%dA%dB", A, B);
 	}
 	
 
