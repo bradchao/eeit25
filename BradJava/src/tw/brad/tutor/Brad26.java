1cdb777930ee0c541d69c5b27c9ebe3829c981b3
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-28 12:00:04 +0800

    08281159

---
+++ BradJava/src/tw/brad/tutor/Brad26.java
@@ -0,0 +1,22 @@
+package tw.brad.tutor;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class Brad26 {
+
+	public static void main(String[] args) {
+		TreeSet<Integer> lottery = new TreeSet<>();
+		//System.out.println(lottery.size());
+		while (lottery.size() < 6) {
+			lottery.add((int)(Math.random()*49+1));
+		}
+		System.out.println(lottery);
+		
+		for (Integer i : lottery) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
