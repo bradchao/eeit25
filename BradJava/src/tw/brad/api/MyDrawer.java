fc82ff8599eab06a914da590cbad6d070ebde8e6
mickeyyo55b <mickeyyo55b@gmail.com>
2026-08-31 11:17:08 +0800

    08311117

--- BradJava/src/tw/brad/api/MyDrawer.java
+++ BradJava/src/tw/brad/api/MyDrawer.java
@@ -1,22 +1,64 @@
 package tw.brad.api;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
+import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel{
 	public MyDrawer() {
 		setBackground(Color.YELLOW);
+		
+		MyListener myListener = new MyListener();
+		addMouseListener(myListener);
+		addMouseMotionListener(myListener);
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		
+//		if (g instanceof DebugGraphics) {
+//			System.out.println("1");
+//		}else if (g instanceof Graphics2D) {
+//			System.out.println("2");
+//		}else {
+//			System.out.println("3");
+//		}
+
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setStroke(new BasicStroke(4));
+		g2d.setColor(Color.BLUE);
+		g2d.drawLine(0, 0, 200, 300);
 		
 	}
+}
+
+class MyListener extends MouseAdapter {
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		super.mousePressed(e);
+		System.out.println("mousePressed");
+	}
 	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		super.mouseDragged(e);
+		System.out.println("mouseDragged");
+	}
 	
 }
+
+
+
+
+
