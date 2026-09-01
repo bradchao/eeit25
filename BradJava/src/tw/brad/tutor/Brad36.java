accba5419f03c5ca935f0adf273ddc68ade3a2fa
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-01 10:03:16 +0800

    09011003

---
+++ BradJava/src/tw/brad/tutor/Brad36.java
@@ -0,0 +1,12 @@
+package tw.brad.tutor;
+
+import java.io.File;
+
+public class Brad36 {
+
+	public static void main(String[] args) {
+		System.out.println(File.pathSeparator);
+		System.out.println(File.separator);
+	}
+
+}
