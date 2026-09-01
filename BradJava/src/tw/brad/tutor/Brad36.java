d184ae65306768b4b7debd5d3ae67d65b29722af
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-01 11:09:58 +0800

    09011109

--- BradJava/src/tw/brad/tutor/Brad36.java
+++ BradJava/src/tw/brad/tutor/Brad36.java
@@ -7,6 +7,15 @@ public class Brad36 {
 	public static void main(String[] args) {
 		System.out.println(File.pathSeparator);
 		System.out.println(File.separator);
+		
+		File dir = new File("c:/brad/dir2");
+		System.out.println(dir.exists());
+		
+		File root = new File(".");
+		System.out.println(root.getAbsolutePath());
+		File dir1 = new File("./dir3");
+		System.out.println(dir1.exists());
+				
 	}
 
 }
