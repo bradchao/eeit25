5f60f1b11706bf5157a3e3002f3964778e8f3234
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-07 15:19:40 +0800

    09071519

--- JDBC/src/tw/brad/tutor/JDBC14.java
+++ JDBC/src/tw/brad/tutor/JDBC14.java
@@ -1,5 +1,11 @@
 package tw.brad.tutor;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
 /*
 SELECT o.EmployeeID, e.FirstName, e.LastName, SUM(od.UnitPrice*od.Quantity) sum 
 FROM `orders` o
@@ -24,9 +30,37 @@ ORDER BY sum DESC
 
  */
 public class JDBC14 {
-
+	private static final String URL = "jdbc:mysql://localhost:3306/north";
+	private static final Properties PROP = new Properties();
+	private static final String SQL_QUERY = """
+							SELECT o.EmployeeID, e.FirstName, e.LastName, SUM(od.UnitPrice*od.Quantity) sum 
+							FROM `orders` o
+								JOIN employees e ON o.EmployeeID = e.EmployeeID
+								JOIN orderdetails od ON o.OrderID = od.OrderID
+							GROUP BY o.EmployeeID
+							ORDER BY sum DESC
+							""";
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		PROP.put("user", "root");
+		PROP.put("password", "root");
+		PROP.put("useSSL", "false");
+		PROP.put("characterEncoding", "UTF-8");
+		
+		try (Connection conn = DriverManager.getConnection(URL,PROP);
+				PreparedStatement pstmt = conn.prepareStatement(SQL_QUERY);){
+			ResultSet rs = pstmt.executeQuery();
+			int rank = 1;
+			while (rs.next()) {
+				System.out.printf("%d : %s %s : %s\n", rank++, 
+						rs.getString("FirstName"), rs.getString("LastName"), rs.getString("sum"));
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+		
 
 	}
 
