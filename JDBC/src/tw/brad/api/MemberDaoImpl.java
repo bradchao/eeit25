daf0c3ed779a3e10ab5f30abcac4a2ad2cdde375
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-08 14:15:42 +0800

    09081415

--- JDBC/src/tw/brad/api/MemberDaoImpl.java
+++ JDBC/src/tw/brad/api/MemberDaoImpl.java
@@ -1,5 +1,9 @@
 package tw.brad.api;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -31,47 +35,96 @@ public class MemberDaoImpl implements MemberDao{
 			);
 	
 	private static final String SQL_ADD = "INSERT INTO member (account,passwd,name) VALUES (?,?,?)";
-	private static final String SQL_UPDATE = "";
-	private static final String SQL_DELETE = "";
-	private static final String SQL_QUERY_ID = "";
-	private static final String SQL_QUERY_ALL = "";
-	private static final String SQL_QUERY_ACCOUNT = "";
-	
+	private static final String SQL_UPDATE = "UPDATE member SET pssswd = ? WHERE id = ?";
+	private static final String SQL_DELETE = "DELETE FROM member WHERE id = ?";
+	private static final String SQL_QUERY_ID = "SELECT id, account, passwd, name FROM member WHERE id = ?";
+	private static final String SQL_QUERY_ALL = "SELECT id, account, passwd, name FROM member";
+	private static final String SQL_QUERY_ACCOUNT = "SELECT id, account, passwd, name FROM member WHERE account = ?";
 	
 	@Override
 	public boolean addMember(Member member) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		if (findByAccount(member.getAccount()) != null) {return false;}
+		
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SQL_ADD)){
+			pstmt.setString(1, member.getAccount());
+			pstmt.setString(2, BCrypt.hashpw(member.getPasswd(), BCrypt.gensalt()));
+			return pstmt.executeUpdate() > 0;
+		}
 	}
 
 	@Override
 	public boolean updateMember(Member member) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SQL_UPDATE)){
+			pstmt.setString(1, BCrypt.hashpw(member.getPasswd(), BCrypt.gensalt()));
+			pstmt.setLong(2, member.getId());
+			return pstmt.executeUpdate() > 0;
+		}	
 	}
 
 	@Override
 	public boolean delMember(Member member) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SQL_DELETE)){
+			pstmt.setLong(1, member.getId());
+			return pstmt.executeUpdate() > 0;
+		}	
 	}
 
 	@Override
 	public Member findById(long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SQL_QUERY_ID)){
+			pstmt.setLong(1, id);
+			try(ResultSet rs = pstmt.executeQuery()){
+				if (rs.next()) {
+					Member member = MEMBER_MAPPER.mapRow(rs);
+					return member;
+				}
+			}
+			return null;
+		}	
 	}
 
 	@Override
 	public List<Member> findAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		List<Member> list = new ArrayList<Member>();
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SQL_QUERY_ALL)){
+			try(ResultSet rs = pstmt.executeQuery()){
+				while (rs.next()) {
+					Member member = MEMBER_MAPPER.mapRow(rs);
+					list.add(member);
+				}
+			}
+			return list;
+		}		
 	}
 
+	private Member findByAccount(String account) throws Exception {
+		try(Connection conn = dataSource.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SQL_QUERY_ACCOUNT)){
+			pstmt.setString(1, account);
+			try(ResultSet rs = pstmt.executeQuery()){
+				if (rs.next()) {
+					Member member = MEMBER_MAPPER.mapRow(rs);
+					return member;
+				}
+			}
+			return null;
+		}	
+	}
+	
+	
 	@Override
 	public Member login(String account, String passwd) throws Exception {
-		// TODO Auto-generated method stub
+		Member member = findByAccount(account);
+		if (member != null && BCrypt.checkpw(passwd, member.getPasswd())) {
+			return member;
+		}
 		return null;
 	}
 
+	
 }
