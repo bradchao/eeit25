957685d4f01ae12ab015f7051213859484aea6ed
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-09 12:00:22 +0800

    09091200

--- BradWeb/src/main/java/tw/brad/servlet/Brad05.java
+++ BradWeb/src/main/java/tw/brad/servlet/Brad05.java
@@ -14,17 +14,28 @@ public class Brad05 extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		String sr = "";
+		if (x != null && y != null) {
+			int r = Integer.parseInt(x) + Integer.parseInt(y);
+			sr += r;
+		}else {
+			x = y = "";
+		}
+		
+		//-----------------------------
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.print("<!DOCTYPE html>\n");
 		out.print("<h1>Brad Big Company</h1>\n");
 		out.print("<hr />\n");
 		out.print("<form action='Brad05'>\n");
-		out.print("<input type='number' name='x'/>\n");
+		out.printf("<input name='x' value='%s' />\n", x);
 		out.print("+\n");
-		out.print("<input name='y'/>\n");
+		out.printf("<input name='y' value='%s' />\n", y);
 		out.print("<input type='submit' value='=' />\n");
-		out.print("<span></span>\n");
+		out.printf("<span>%s</span>\n", sr);
 		out.print("</form>\n");
 		
 	}
