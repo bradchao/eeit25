113bfc51d2a0b85517b3de65e6685be12be1eebd
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-09 14:48:39 +0800

    09091448

---
+++ BradWeb/src/main/java/tw/brad/servlet/Brad06.java
@@ -0,0 +1,30 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet("/Brad06")
+public class Brad06 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		System.out.println("doGet");
+		String[] habits = request.getParameterValues("habit");
+		for (String habit : habits)System.out.println(habit);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String account = request.getParameter("account");
+		String passwd = request.getParameter("passwd");
+		System.out.println("doPost");
+		System.out.println(account);
+		System.out.println(passwd);
+	}
+
+}
