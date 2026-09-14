33608553c78ff058e240499d04b631ce3a1f2ffb
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-14 14:28:44 +0800

    09141428

--- BradWeb/src/main/webapp/js/teacher.js
+++ BradWeb/src/main/webapp/js/teacher.js
@@ -1,10 +1,30 @@
 window.onload = function(){
+	let url = "ws://10.0.100.160:8080/BradWeb/mycenter";
+	let webSocket = new WebSocket(url);
+	
 	let clear = document.getElementById("clear");
 	let myDrawer = document.getElementById("myDrawer"); 
 	
 	let ctx = myDrawer.getContext("2d");
 	let isDrag = false;
+	let isConnect = false;
+
+	//-------------------------------------------
 	
+	webSocket.onopen = function(){
+		isConnect = true;
+		let data = {
+			isTeacher : true
+		};
+		webSocket.send(JSON.stringify(data));
+		
+	}
+	
+	webSocket.onclose = function(){
+		isConnect = false;
+	}
+	
+	//------------------------------------------
 	myDrawer.onmousedown = function(e){
 		isDrag = true;
 		let x = e.offsetX, y = e.offsetY;
@@ -13,6 +33,14 @@ window.onload = function(){
 		ctx.lineWidth = 4;
 		ctx.beginPath();
 		ctx.moveTo(x, y);
+		
+		let data = {
+			isClear : false,
+			isNewLine : true,
+			x : x,
+			y : y
+		};
+		webSocket.send(JSON.stringify(data));
 		
 	}
 	
@@ -28,11 +56,27 @@ window.onload = function(){
 			ctx.lineTo(x, y);
 			ctx.stroke();
 			
+			let data = {
+				isClear : false,
+				isNewLine : false,
+				x : x,
+				y : y
+			};
+			webSocket.send(JSON.stringify(data));
+			
+			
 		}
 	}
 	
 	clear.addEventListener("click", function(){
 		ctx.clearRect(0,0, myDrawer.width, myDrawer.height);
+		
+		let data = {
+			isClear : true,
+		};
+		webSocket.send(JSON.stringify(data));
+		
+		
 	});
 	
 }
