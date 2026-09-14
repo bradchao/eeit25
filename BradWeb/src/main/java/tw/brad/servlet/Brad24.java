59de304918c831f343aef2ae3aea6cb39ebcf781
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-14 16:11:41 +0800

    09141611

--- BradWeb/src/main/java/tw/brad/servlet/Brad24.java
+++ BradWeb/src/main/java/tw/brad/servlet/Brad24.java
@@ -10,8 +10,38 @@ import java.io.PrintWriter;
 
 @WebServlet("/Brad24")
 public class Brad24 extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.service(req, resp);
+		
+		System.out.println(request.getMethod());
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String max = request.getParameter("max");
+		int lottery = 0;
+		if (max != null) {
+			try {
+				lottery = (int)(Math.random()*Integer.parseInt(max)+1);
+			}catch(Exception e) {
+				
+			}
+		}else {
+			lottery = (int)(Math.random()*49+1);
+		}
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(lottery);
+		
+	}
+
+	/*
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		
 		request.setCharacterEncoding("UTF-8");
 		
 		String max = request.getParameter("max");
@@ -30,5 +60,5 @@ public class Brad24 extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.print(lottery);
 	}
-
+	*/
 }
