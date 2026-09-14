06e609708b82fc8aa6185125b8a93cd7a8ead830
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-14 15:46:38 +0800

    09141546

---
+++ BradWeb/src/main/java/tw/brad/servlet/Brad24.java
@@ -0,0 +1,22 @@
+package tw.brad.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Brad24")
+public class Brad24 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print((int)(Math.random()*49+1));
+	}
+
+}
