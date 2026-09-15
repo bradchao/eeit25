213eb15ccbf5af1812f0b2399b05a0e1dc386e8d
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-15 15:23:38 +0800

    09151523

--- BradWeb/src/main/java/tw/brad/api/BradUtil.java
+++ BradWeb/src/main/java/tw/brad/api/BradUtil.java
@@ -3,6 +3,7 @@ package tw.brad.api;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.util.Random;
 
 public class BradUtil {
 	public static String calc(String x, String y) {
@@ -13,6 +14,10 @@ public class BradUtil {
 		}catch(Exception e) {
 			return "";
 		}
+	}
+	
+	public static int createScore() {
+		return new Random().nextInt(101);
 	}
 	
 	public static String loadView(String source) throws Exception{
