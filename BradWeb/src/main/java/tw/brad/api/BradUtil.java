41828dfef91a5d73d0b59116ad61bcd57cd1bd09
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-16 09:32:16 +0800

    09160932

--- BradWeb/src/main/java/tw/brad/api/BradUtil.java
+++ BradWeb/src/main/java/tw/brad/api/BradUtil.java
@@ -5,6 +5,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.util.Random;
 import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class BradUtil {
 	public static String calc(String x, String y) {
@@ -22,7 +26,20 @@ public class BradUtil {
 	}
 	
 	public static SortedMap[] parseHotels(String json) {
-		return null;
+		JSONArray root = new JSONArray(json);
+		TreeMap<String, String>[] hotels = new TreeMap[root.length()];
+		
+		for (int i=0; i<root.length(); i++) {
+			JSONObject hotel = root.getJSONObject(i);
+			TreeMap<String, String> map = new TreeMap<String, String>();
+			map.put("name", hotel.getString("Name"));
+			map.put("addr", hotel.getString("Address"));
+			map.put("tel", hotel.getString("Tel"));
+			hotels[i] = map;
+		}
+		
+		
+		return hotels;
 	}
 	
 	public static String loadView(String source) throws Exception{
