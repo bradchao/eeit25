40bc967ba844f0ef5aec960718efa48bcf5300d0
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-17 09:54:32 +0800

    09170954

---
+++ hibernate/src/main/java/tw/brad/hibernate/tutor/Brad07.java
@@ -0,0 +1,33 @@
+package tw.brad.hibernate.tutor;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import jakarta.persistence.Id;
+import tw.brad.hibernate.entity.Account;
+
+public class Brad07 {
+
+	public static void main(String[] args) {
+		Class<Account> aClass = Account.class;
+		
+		System.out.println(aClass);
+		System.out.println(aClass.getName());
+		System.out.println(aClass.getSimpleName());
+		
+		System.out.println("-----");
+		Field[] fields = aClass.getDeclaredFields();
+		for (Field field: fields) {
+			System.out.println(field.getName() + ":" + 
+						field.getType().getName() + ":" 
+						+ (field.isAnnotationPresent(Id.class)?"ID":""));
+		}
+		System.out.println("-----");
+		Method[] methods = aClass.getDeclaredMethods();
+		for (Method method: methods) {
+			System.out.println(method.getName());
+		}
+		
+	}
+
+}
---
+++ hibernate/src/main/java/tw/brad/hibernate/util/HibernateUtil.java
@@ -0,0 +1,20 @@
+package tw.brad.hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration cfg = new Configuration();
+			cfg.configure("hibernate.cfg.xml");
+			
+			sessionFactory = cfg.buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+	
+	
+}
