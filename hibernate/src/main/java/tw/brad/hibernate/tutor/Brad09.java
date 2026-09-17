469fede5c697aeb549ec88118403b0bc1345fb6d
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-17 10:38:59 +0800

    09171038

--- hibernate/src/main/java/tw/brad/hibernate/tutor/Brad09.java
+++ hibernate/src/main/java/tw/brad/hibernate/tutor/Brad09.java
@@ -1,28 +1,29 @@
 package tw.brad.hibernate.tutor;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
 
 import tw.brad.hibernate.util.HibernateUtil;
 
-public class Brad08 {
+public class Brad09 {
 	public static void main(String[] args) {
 		try (Session session = HibernateUtil.getSessionFactory().openSession()){
 			Transaction transaction = session.beginTransaction();
 
 			String sql = """
-					SELECT * FROM account
+					INSERT INTO cust
+						(cname, tel, birthday)
+					VALUES
+						(:cname, :tel, :birth)
 					""";
 			NativeQuery query = session.createNativeQuery(sql);
-			List list = query.getResultList();
-			System.out.println(list.size());
-			for (Object obj : list) {
-				Object[] row = (Object[])obj;
-				System.out.printf("%s:%s:%s:%s:%s\n", row[0],row[1],row[2],row[3],row[4]);
-			}
+			query.setParameter("cname", "test1");
+			query.setParameter("tel", "123");
+			query.setParameter("birth", "1999-01-02");
+			
+			int n = query.executeUpdate();
+			System.out.println(n);
 			
 			transaction.commit();
 		}
