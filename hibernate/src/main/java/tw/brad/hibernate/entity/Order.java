084fc02de56eb5520ca27355e33ab7c24489139c
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-18 11:09:44 +0800

    09181109

--- hibernate/src/main/java/tw/brad/hibernate/entity/Order.java
+++ hibernate/src/main/java/tw/brad/hibernate/entity/Order.java
@@ -1,12 +1,16 @@
 package tw.brad.hibernate.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -41,6 +45,30 @@ public class Order {
 		this.odate = odate;
 	}
 	//----------------------------
+	@OneToMany(mappedBy = "order", 
+				cascade = CascadeType.ALL,
+				orphanRemoval = true)
+	private List<OrderItem> items = new ArrayList<>();
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+	
+	public void addItem(OrderItem item) {
+		if (item != null) {
+			items.add(item);
+			item.setOrder(this);
+		}
+	}
+	public void removeItem(OrderItem item) {
+		if (item != null && items.contains(item)) {
+			items.remove(item);
+			item.setOrder(null);
+		}
+	}
 	
 	
 }
--- hibernate/src/main/java/tw/brad/hibernate/entity/OrderItem.java
+++ hibernate/src/main/java/tw/brad/hibernate/entity/OrderItem.java
@@ -2,9 +2,12 @@ package tw.brad.hibernate.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class OrderItem {
@@ -54,5 +57,18 @@ public class OrderItem {
 		this.qty = qty;
 	}
 	//-------------------
+	@ManyToOne(fetch = FetchType.EAGER)
+	//@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "oid", nullable = false)
+	private Order order;
+
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
 	
 }
