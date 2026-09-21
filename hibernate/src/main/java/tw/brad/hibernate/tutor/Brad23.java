59f173918e94aa13fd287737df198892a41a00d5
mickeyyo55b <mickeyyo55b@gmail.com>
2026-09-21 14:17:45 +0800

    09211417

--- hibernate/src/main/java/tw/brad/hibernate/tutor/Brad23.java
+++ hibernate/src/main/java/tw/brad/hibernate/tutor/Brad23.java
@@ -2,6 +2,7 @@ package tw.brad.hibernate.tutor;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import tw.brad.hibernate.dao.SCDao;
 import tw.brad.hibernate.entity.Course;
@@ -19,24 +20,31 @@ public class Brad23 {
 		long sid = scanner.nextLong();
 		
 		s1 = dao.getStudentById(sid);
-		System.out.printf("Welcome, %s(%d)", s1.getSname(), s1.getId());
-		
-		List<Course> courses = dao.getAllCourse();
-		for (Course course: courses) {
-			System.out.printf("%d: %s\n", course.getId(), course.getCname());
+		System.out.printf("Welcome, %s(%d)\n", s1.getSname(), s1.getId());
+
+		while (true) {
+			List<Course> courses = dao.getAllCourse();
+			for (Course course: courses) {
+				if (!isExist(s1, course.getStudents())) {
+					System.out.printf("%d: %s\n", course.getId(), course.getCname());
+				}
+			}
+			
+			System.out.println("----");
+			System.out.print("Course ID(0: exit):");
+			long cid = scanner.nextLong();
+
+			if (cid <= 0) break;
+			
+			s1.addCourse(dao.getCourseById(cid));
+			s1 = dao.update(s1);
 		}
-		
-		System.out.println("----");
-		System.out.print("Course ID:");
-		long cid = scanner.nextLong();
-		
-		s1.addCourse(dao.getCourseById(cid));
-		s1 = dao.update(s1);
-		
-		
-		
-		
-		
 	}
+	
+	private static boolean isExist(Student s, Set<Student> students) {
+		return students.stream().anyMatch(student -> student.getId().equals(s.getId()));
+	}
+	
+	
 
 }
